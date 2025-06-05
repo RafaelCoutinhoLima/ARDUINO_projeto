accaeafc18e6fe573b4b9efb2e474e41a21b49c7
RAFAEL COUTINHO LIMA <rafaelcoutinholima12345@gmail.com>
2025-06-04 22:31:50 -0300

    Update ARDUINO_projeto6.ino

--- ARDUINO_projeto6.ino
+++ ARDUINO_projeto6.ino
@@ -1,1 +1,19 @@
+//PUZZLE DAS TEXTURAS E DAS EMOÇÕES
+const int ledPin = 7; 
+const int buttonPin = 3;
 
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT); 
+
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    delay(2000);
+    digitalWrite(ledPin, LOW); 
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
